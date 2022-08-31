afab8ce7e0fbe81c11c161a6b69ecea2cce8ef00
jarro06 <102299007+jarro06@users.noreply.github.com>
2022-08-31 14:50:03 +0200

    Create gradle project

---
+++ src/main/java/de/allianz/project_saved_in_config/ProjectSavedInConfigApplication.java
@@ -0,0 +1,13 @@
+package de.allianz.project_saved_in_config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSavedInConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSavedInConfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/allianz/project_saved_in_config/ProjectSavedInConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package de.allianz.project_saved_in_config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSavedInConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
